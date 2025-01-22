4e253afb82d2a431bcb691c810976e532640e765
ramirosanchezaranda <ramirosanchezaranda@gmail.com>
2025-01-22 19:00:03 -0300

    first-commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          'blue': {
+            600: '#2563eb',
+            700: '#1d4ed8'
+          }
+        }
+      }
+    }
+  }
